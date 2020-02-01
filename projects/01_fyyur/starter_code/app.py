3b8e1f8997a348d16652cf651ac9df224d8fc43c
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-02-01 01:44:27 +0000

    feat: Set up models Venue & Artist using migration
    In order to do this, first we had to connect our app to a db.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -22,6 +23,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+# DONE
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# DONE
+SQLALCHEMY_DATABASE_URI = 'postgresql://alba@localhost:5432/fyyurapp'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
