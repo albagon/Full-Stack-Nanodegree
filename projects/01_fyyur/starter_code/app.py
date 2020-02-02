d9b07bf5c73e239109e93594cd3f4adddceba59f
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-02-02 16:11:51 +0000

    feat: Add fields to create venue template & route

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -253,13 +253,40 @@ def create_venue_form():
 def create_venue_submission():
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+  error = False
+  try:
+    form = request.form
+    venue = Venue(
+      name= form['name'],
+      genres = form['genres'],
+      address = form['address'],
+      city = form['city'],
+      state = form['state'],
+      phone = form['phone'],
+      facebook_link = form['facebook_link'],
+      website = form['website'],
+      image_link = form['image_link'],
+      seeking_talent = form['seeking_talent'],
+      seeking_description = form['seeking_description']
+    )
+  except:
+    error = True
+    #db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    #db.session.close()
+    # on successful db insert, flash success
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    #flash('Venue ' + request.form['genres'] + ' was successfully listed!')
+  if not error:
+    #return jsonify(body)
+    return render_template('pages/home.html')
+  else:
+    flash('Venue ' + form['name'] + ' was NOT successfully listed!')
+    #abort(500)
+    # TODO: on unsuccessful db insert, flash an error instead.
+    # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
+    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, AnyOf, URL
 
 class ShowForm(Form):
@@ -115,6 +115,23 @@ class VenueForm(Form):
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
+    )
+    website = StringField(
+        'website', validators=[URL()]
+    )
+    image_link = StringField(
+        'image_link', validators=[URL()]
+    )
+    seeking_talent = SelectField(
+        'seeking_talent',
+        validators=[DataRequired()],
+        choices=[
+            ('Yes', 'Yes'),
+            ('No', 'No')
+        ]
+    )
+    seeking_description = TextAreaField(
+        'seeking_description'
     )
 
 class ArtistForm(Form):
