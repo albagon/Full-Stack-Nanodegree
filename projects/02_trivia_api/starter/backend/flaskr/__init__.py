35852e7cb4fbb91dc2ed7787cf635a13615d28a0
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-02-11 11:32:39 +0000

    feat: Define paginate questions helper method

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,19 +8,39 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+  """ This is a helper method that retrieves only a set of questions based on
+  the page number.
+
+  Arguments:
+    request -- the clients request that contains a page number
+    selection -- the list of questions we want to paginate
+  Return:
+    The set of formatted questions that correspond to the page number in the
+    request
+  """
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
 
-  '''
+  """
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  """
   CORS(app, resources={r'/*': {'origins': '*'}})
 
-  '''
+  """
   @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  """
   @app.after_request
   def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
@@ -28,11 +48,11 @@ def create_app(test_config=None):
     return response
 
 
-  '''
+  """
   @TODO:
   Create an endpoint to handle GET requests
   for all available categories.
-  '''
+  """
   @app.route('/categories')
   def get_categories():
     categories = Category.query.all()
@@ -43,7 +63,7 @@ def create_app(test_config=None):
       'total_categories': len(formatted_categories)
     })
 
-  '''
+  """
   @TODO:
   Create an endpoint to handle GET requests for questions,
   including pagination (every 10 questions).
@@ -54,16 +74,12 @@ def create_app(test_config=None):
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions.
-  '''
+  """
 
   @app.route('/questions')
   def get_questions():
-    page = request.args.get('page', 1, type=int)
-    start = (page - 1) * QUESTIONS_PER_PAGE
-    end = start + QUESTIONS_PER_PAGE
-    questions = Question.query.all()
-    formatted_questions = [question.format() for question in questions]
-    current_questions = formatted_questions[start:end]
+    selection = Question.query.order_by(Question.id).all()
+    current_questions = paginate_questions(request, selection)
 
     if len(current_questions) == 0:
       abort(404)
@@ -73,21 +89,21 @@ def create_app(test_config=None):
 
     return jsonify({
       'success': True,
-      'questions': formatted_questions[start:end],
-      'total_questions': len(formatted_questions),
-      'current_category': questions[start].category,
+      'questions': current_questions,
+      'total_questions': len(selection),
+      'current_category': current_questions[0]['category'],
       'categories': formatted_categories
     })
 
-  '''
+  """
   @TODO:
   Create an endpoint to DELETE question using a question ID.
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page.
-  '''
+  """
 
-  '''
+  """
   @TODO:
   Create an endpoint to POST a new question,
   which will require the question and answer text,
@@ -96,9 +112,9 @@ def create_app(test_config=None):
   TEST: When you submit a question on the "Add" tab,
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.
-  '''
+  """
 
-  '''
+  """
   @TODO:
   Create a POST endpoint to get questions based on a search term.
   It should return any questions for whom the search term
@@ -107,19 +123,19 @@ def create_app(test_config=None):
   TEST: Search by any phrase. The questions list will update to include
   only question that include that string within their question.
   Try using the word "title" to start.
-  '''
+  """
 
-  '''
+  """
   @TODO:
   Create a GET endpoint to get questions based on category.
 
   TEST: In the "List" tab / main screen, clicking on one of the
   categories in the left column will cause only questions of that
   category to be shown.
-  '''
+  """
 
 
-  '''
+  """
   @TODO:
   Create a POST endpoint to get questions to play the quiz.
   This endpoint should take category and previous question parameters
@@ -129,13 +145,13 @@ def create_app(test_config=None):
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not.
-  '''
+  """
 
-  '''
+  """
   @TODO:
   Create error handlers for all expected errors
   including 404 and 422.
-  '''
+  """
 
   @app.errorhandler(404)
   def not_found(error):
