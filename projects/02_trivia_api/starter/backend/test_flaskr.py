2686125b3b7862319e56ec290a57baea375c17b4
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-02-11 11:44:18 +0000

    style: Change indentation to 4 spaces

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -14,8 +14,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = 'trivia_test'
+        self.database_path = 'postgres://{}/{}'.format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -30,7 +30,7 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
+    @TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
     def test_get_categories(self):
