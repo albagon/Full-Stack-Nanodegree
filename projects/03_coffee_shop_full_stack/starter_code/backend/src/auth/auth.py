665f1a730eec9fcc8c93b0596fdbe67b17a08919
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-03-15 00:27:11 +0000

    fix: Import abort from flask

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
